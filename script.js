81548f39a2adfaed7bd4ef7a66474eb3df07c035
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-07-16 23:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this.species = species;
-	}
-	get makeSound(){
-		return `The ${this.species} makes a sound`; 
-	}
+	constructor(species) {
+        this._species = species;
+    }
+
+    // Getter for species
+    get species() {
+        return this._species;
+    }
+
+    // Method to make a sound (can be overridden by subclasses)
+    makeSound() {
+        console.log("Some generic animal sound");
+    }
 }
 
 class Dog extends Animal {
-	bark(){
-		console.log("woof");
-	}
+	constructor(species) {
+        super(species);
+    }
+
+    // Method to purr
+    purr() {
+        console.log("purr");
+    }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
-}
-const myCat = new Cat("Siamese");
-console.log(myCat.makeSound); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
+	 constructor(species) {
+        super(species);
+    }
 
-const myDog = new Dog("Golden Retriever");
-console.log(myDog.makeSound); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woofof
+    // Method to bark
+    bark() {
+        console.log("woof");
+    }
+}
+const animal = new Animal("generic");
+console.log(animal.species); // Output: generic
+animal.makeSound(); // Output: Some generic animal sound
+
+const cat = new Cat("feline");
+console.log(cat.species); // Output: feline
+cat.makeSound(); // Output: Some generic animal sound (inherited from Animal)
+cat.purr(); // Output: purr
+
+const dog = new Dog("canine");
+console.log(dog.species); // Output: canine
+dog.makeSound(); // Output: Some generic animal sound (inherited from Animal)
+dog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
